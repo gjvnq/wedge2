bb98330d86d8d21b0a9ffc43c2fbe089c03187bf
Gabriel Queiroz <gabrieljvnq@gmail.com>
2017-09-21 11:42:14 -0300

    Added a few more tests.

--- test/wedge2/core_test.clj
+++ test/wedge2/core_test.clj
@@ -3,7 +3,19 @@
             [wedge2.core :refer :all]
             [clj-time.core :as t]))
 
-(deftest test-sort-movements
+(deftest test-sort-movements-1
+	(let [
+		input  []
+		output []]
+    (is (= output (sort-movements input)))))
+
+(deftest test-sort-movements-2
+	(let [
+		input  [{:date (t/local-date 2017 1 2)}]
+		output [{:date (t/local-date 2017 1 2)}]]
+    (is (= output (sort-movements input)))))
+
+(deftest test-sort-movements-3
 	(let [
 		input  [{:date (t/local-date 2017 1 2)} {:date (t/local-date 2017 3 2)} {:date (t/local-date 2014 9 2)}]
 		output [{:date (t/local-date 2014 9 2)} {:date (t/local-date 2017 1 2)} {:date (t/local-date 2017 3 2)}]]
@@ -16,6 +28,12 @@
     (is (= output (balances input)))))
 
 (deftest test-balances-2
+	(let [
+		input  [{:account "A" :date (t/local-date 2017 1 2) :value 3/4}]
+		output {"A" [{:date (t/local-date 2017 1 2) :value 3/4}]}]
+    (is (= output (balances input)))))
+
+(deftest test-balances-3
 	(let [
 		input  [{:account "A" :date (t/local-date 2017 1 2) :value 3/4} {:account "B" :date (t/local-date 2017 3 2) :value 20} {:account "B" :date (t/local-date 2014 9 2) :value -1/4}]
 		output {"A" [{:date (t/local-date 2017 1 2) :value 3/4}] "B" [{:date (t/local-date 2014 9 2) :value -1/4} {:date (t/local-date 2017 3 2) :value 79/4}]}]

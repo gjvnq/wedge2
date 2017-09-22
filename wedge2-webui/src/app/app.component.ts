c4cadae641b9193bd528882848b7d013b26f50a4
Gabriel Queiroz <gabrieljvnq@gmail.com>
2017-09-22 18:33:31 -0300

    Started exploring Angular.

--- wedge2-webui/src/app/app.component.ts
+++ wedge2-webui/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'wedge²-webui';
+  hero: Hero = {
+  	id: 2,
+  	name: "Olá"
+  };
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }

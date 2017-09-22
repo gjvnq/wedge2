3b30abd9a0f439515262611a6d3dc7b4fe499780
Gabriel Queiroz <gabrieljvnq@gmail.com>
2017-09-22 18:14:13 -0300

    Fixed licensing.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject wedge2 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "LGPL"
+            :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"] [cheshire "5.8.0"] [clj-time "0.14.0"]]
   :main ^:skip-aot wedge2.core
   :target-path "target/%s"

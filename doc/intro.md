c8895c06f7eab95ff4c18cd640c3aae0905a5843
Gabriel Queiroz <gabrieljvnq@gmail.com>
2017-10-06 09:26:29 -0300

    Clean up

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to wedge2
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- src/wedge2/rates-coinmarketcap.clj
+++
@@ -1,5 +0,0 @@
-(in-ns 'wedge2.core)
-
-(defn conv-coinmarketcap
-	[{date :date asset-src :asset-src asset-dst :asset-dst}]
-	{:error "not implemented" :factor 0 :asset "BTC"})
